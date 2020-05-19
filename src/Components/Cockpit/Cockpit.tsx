fcdb42b251dffa4deae3e9b381c9d66dd5df1630
Michael Williams <Michael.Williams@ama.ab.ca>
2020-05-19 17:48:16 -0600

    useEffect implemented - illustrates Componant Lifecycle

--- src/Components/Cockpit/Cockpit.tsx
+++ src/Components/Cockpit/Cockpit.tsx
@@ -1,10 +1,29 @@
-import React, { useContext } from 'react';
+import React, { useEffect, useContext } from 'react';
 import AuthContext from '../../context/AuthContext'
 
 const Cockpit = (props: any) => {
 
     const authContext = useContext(AuthContext);
     console.log(authContext.authenticated);
+
+    useEffect(() => {
+        console.log('[Cockpit.tsx] useEffect');
+        // Http request...
+        // setTimeout(() => {
+        //   alert('Saved data to cloud!');
+        // }, 1000);
+        // toggleBtnRef.current.click();
+        return () => {
+            console.log('[Cockpit.tsx] cleanup work in useEffect');
+        };
+    }, []);
+
+    useEffect(() => {
+        console.log('[Cockpit.tsx] 2nd useEffect');
+        return () => {
+            console.log('[Cockpit.tsx] cleanup work in 2nd useEffect');
+        };
+    });
 
     return (
         <div>
